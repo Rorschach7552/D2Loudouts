9e9fc7868191406f150a20ea3d80c42d0c9cc758
ahmedmgamal94 <98055904+ahmedmgamal94@users.noreply.github.com>
2024-05-22 13:00:10 -0700

    added value range / table components
     added value range / table components

---
+++ D2loadout/src/App.tsx
@@ -0,0 +1,21 @@
+// import DiscreteSlider from "./Components/DiscreteSlider";
+// function App() {
+//   return (
+//     <div>
+//       <DiscreteSlider />
+//     </div>
+//   );
+// }
+// export default App;
+import React from "react";
+import StatsTable from "./Components/StatsTable";
+
+function App() {
+  return (
+    <div>
+      <StatsTable />
+    </div>
+  );
+}
+
+export default App;
